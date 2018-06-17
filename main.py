1049601fd7e5a29a4ab325f83d3e2ba56fe22720
Mauro Riva <lemariva@gmail.com>
2018-06-17 13:07:01 +0200

    added license description

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# coding=utf-8
+'''
+ Copyright [2017] [Mauro Riva <lemariva@mail.com> <lemariva.com>]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+'''
+
 import utime
 from uPySensors.imu import MPU6050
 from servo import Servo
